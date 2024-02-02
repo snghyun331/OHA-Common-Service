46b4d7bcaa8bf50cd9fc87f19f09696309c5a2d1
Lee SH <snghyun331@gmail.com>
2024-02-02 21:23:24 +0900

    chore: cron 세팅

--- src/weathers/weather.module.ts
+++ src/weathers/weather.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { WeathersController } from './weather.controller';
 import { WeathersService } from './weather.service';
 import { WeatherEntity } from './entities/weather.entity';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TaskService } from './task.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([WeatherEntity])],
+  imports: [TypeOrmModule.forFeature([WeatherEntity]), ScheduleModule.forRoot()],
   controllers: [WeathersController],
-  providers: [WeathersService],
+  providers: [WeathersService, TaskService],
 })
 export class WeathersModule {}
