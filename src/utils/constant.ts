fd86f5cee2cd5b8f03d5bd5568c10c20f3fa65aa
Lee SH <snghyun331@gmail.com>
2024-02-22 15:17:26 +0900

    add: 유레카 registry fetch  간격 설정

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -13,3 +13,7 @@ export const KOREA_TIME_OFFSET = 9 * 60; // 한국은 UTC+9
 export const KOREA_TIME_OFFEST_TO_MILLIESEC = KOREA_TIME_OFFSET * 60000;
 
 export const EarthRadiusKm = 6371;
+
+export const Eureka_Heartbeat_Interval = 2000000;
+
+export const Eureka_Registery_Interval = 2000000;
