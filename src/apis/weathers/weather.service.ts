88591c4ad1ec4a59d6b5de3a8128407bb36897db
Lee SH <snghyun331@gmail.com>
2024-02-26 14:49:36 +0900

    add: Dockerfile에 timezone 추가

--- src/apis/weathers/weather.service.ts
+++ src/apis/weathers/weather.service.ts
@@ -48,37 +48,42 @@ export class WeathersService {
   }
 
   async getWeatherDatas(userId: number) {
-    const { code } = await this.freqDistrictRepository.findOne({
-      select: { code: true },
-      where: { userId, isDefault: true },
-    });
-    if (!code) {
-      throw new NotFoundException('default인 지역이 없습니다');
-    }
-    const { nx, ny } = await this.locationsService.getGridByCode(code);
-    const currentDateTime = moment().tz('Asia/Seoul');
-    const currentDate = currentDateTime.format('YYYYMMDD');
-    const currentHour = currentDateTime.format('HH:mm');
-    this.logger.verbose(`currentDate: ${currentDate}`);
-    this.logger.verbose(`currentHour: ${currentHour}`);
+    try {
+      const { code } = await this.freqDistrictRepository.findOne({
+        select: { code: true },
+        where: { userId, isDefault: true },
+      });
+      if (!code) {
+        throw new NotFoundException('default인 지역이 없습니다');
+      }
+      const { nx, ny } = await this.locationsService.getGridByCode(code);
+      const currentDateTime = moment().tz('Asia/Seoul');
+      const currentDate = currentDateTime.format('YYYYMMDD');
+      const currentHour = currentDateTime.format('HH:mm');
+      this.logger.verbose(`currentDate: ${currentDate}`);
+      this.logger.verbose(`currentHour: ${currentHour}`);
 
-    const weatherInfos = await this.weatherRepository.findOne({
-      where: { fcstDate: currentDate, fcstTime: currentHour + '00', nx, ny },
-    });
-    if (!weatherInfos) {
-      throw new NotFoundException('날씨 정보가 없습니다');
-    }
+      const weatherInfos = await this.weatherRepository.findOne({
+        where: { fcstDate: currentDate, fcstTime: currentHour + '00', nx, ny },
+      });
+      if (!weatherInfos) {
+        throw new NotFoundException('날씨 정보가 없습니다');
+      }
 
-    const { fcstDate, fcstTime, POP, PTY, REH, SKY, TMP, WSD } = weatherInfos;
-    const date = fcstDate;
-    const hour = fcstTime;
-    const precipPercent = POP;
-    const precipType = await this.getPtyType(PTY);
-    const humidity = REH;
-    const sky = await this.getSkyType(SKY);
-    const hourlyTemp = TMP;
-    const windSpeed = WSD;
-    return { date, hour, precipPercent, precipType, humidity, sky, hourlyTemp, windSpeed };
+      const { fcstDate, fcstTime, POP, PTY, REH, SKY, TMP, WSD } = weatherInfos;
+      const date = fcstDate;
+      const hour = fcstTime;
+      const precipPercent = POP;
+      const precipType = await this.getPtyType(PTY);
+      const humidity = REH;
+      const sky = await this.getSkyType(SKY);
+      const hourlyTemp = TMP;
+      const windSpeed = WSD;
+      return { date, hour, precipPercent, precipType, humidity, sky, hourlyTemp, windSpeed };
+    } catch (e) {
+      this.logger.error(e);
+      throw e;
+    }
   }
 
   private async insertWeather() {
