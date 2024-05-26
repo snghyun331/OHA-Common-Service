65f736afbc29f813be9081e25c2ff98e536af1a5
Lee SH <snghyun331@gmail.com>
2024-05-26 19:49:29 +0900

    Feat: [#54] 사용자가 디폴트로 설정한 지역의 기상청 날씨 조회

---
+++ src/utils/decorators/api-response.dto.ts
@@ -0,0 +1,47 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { KmaType } from 'src/module/weather/enums/kma.enum';
+
+export class WeatherDataDto {
+  @ApiProperty({
+    enum: ['KMA_CLOUDY', 'Moderator', 'User'],
+  })
+  widget: KmaType;
+
+  @ApiProperty({
+    description: 'Probability of precipitation in percentage',
+    example: '30',
+  })
+  probPrecip: string;
+
+  @ApiProperty({
+    description: 'Indicates if the temperature difference is high',
+    example: false,
+  })
+  isTempDiffHigh: boolean;
+
+  @ApiProperty({
+    description: 'Hourly temperature in degrees Celsius',
+    example: '14',
+  })
+  hourlyTemp: string;
+}
+
+export class ApiResponseDto {
+  @ApiProperty({
+    description: 'HTTP status code of the response',
+    example: 200,
+  })
+  statusCode: number;
+
+  @ApiProperty({
+    description: 'Message indicating the success or failure of the request',
+    example: '성공',
+  })
+  message: string;
+
+  @ApiProperty({
+    description: 'The data containing weather information',
+    type: WeatherDataDto,
+  })
+  data: WeatherDataDto;
+}
